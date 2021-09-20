c9894651dc1d13fb86f5069088bb2fe4519d558e
vladhuk <vladhookovskiy@gmail.com>
2021-09-21 01:26:09 +0300

    - init project;

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AmqpModule } from './amqp/amqp.module';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        database: configService.get('DB_NAME'),
+        password: configService.get('DB_PASSWORD'),
+        autoLoadEntities: true,
+        logging: true,
+        synchronize: true,
+      }),
+    }),
+    AmqpModule,
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
