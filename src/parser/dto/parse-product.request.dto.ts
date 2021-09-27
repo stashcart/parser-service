dd28fa1056f4cbd03514479082d7d0a6531887a5
vladhuk <vladhookovskiy@gmail.com>
2021-09-28 02:01:55 +0300

    - add dto;

---
+++ src/parser/dto/parse-product.request.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
+
+export class ParseProductRequestDto {
+  @ApiProperty({
+    example:
+      'https://ua.iherb.com/pr/jarrow-formulas-msm-1-000-mg-200-veggie-caps/244',
+  })
+  @IsUrl()
+  url!: string;
+
+  @IsString()
+  storeName!: string;
+}
